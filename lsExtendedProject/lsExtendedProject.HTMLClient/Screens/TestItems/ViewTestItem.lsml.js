70d7bbec55b661a025fbda46e8cdbc4dee83caf8
dwm9100b <dale@interbaytechnology.com>
2014-04-17 12:06:36 -0700

    Initial commit

---
+++ lsExtendedProject/lsExtendedProject.HTMLClient/Screens/TestItems/ViewTestItem.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewTestItem.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.TestItem.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.TestItem." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
---
+++ lsExtendedProject/lsExtendedProject.Server/default.aspx
@@ -0,0 +1,10 @@
+<%--<%@ Page Language="C#" CodeBehind="default.aspx.cs" Inherits="LightSwitchApplication._Default" %>--%>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+	<h2>Hello World from default.aspx</h2>
+</body>
+</html>
